74851af47dc078d45b40bd913ac981a2db405d14
netblue30 <netblue30@protonmail.com>
2021-07-11 11:42:27 -0400

    importing project files

---
+++ syscheck.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+print_colorized () {
+	printf "\x1b[32m%s %s...\x1b[0m\n" $1 $2
+}
+
+
+print_colorized running apt-update
+sudo apt-get update
+sudo apt-get autoremove
+sudo apt-get -y upgrade
+
+print_colorized checking dpkg
+sudo dpkg --verify | tee database/dpkg--verify
+
+print_colorized checking local-files
+sha512sum ~/.bashrc > database/dot-bashrc
+cat ~/.config/autostart/* | sha512sum > database/dot-config-autostart
+
+print_colorized checking rkhunter
+sudo rkhunter --check --cronjob --disable ipc_shared_mem --report-warnings-only > database/rkhunter
+
+print_colorized checking chkrootkit
+sudo chkrootkit | grep -v '!' > database/chkrootkit
+
+print_colorized all done!
