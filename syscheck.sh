6dd3e3f5eb3db4a8ebed195b1e77a1575b4dc682
netblue30 <netblue30@protonmail.com>
2021-07-12 08:41:27 -0400

    new script, database

--- syscheck.sh
+++ syscheck.sh
@@ -1,79 +1,93 @@
 #!/bin/bash
 
+OUTPUT="output"
+DATABASE="database"
+
 test_file () {
-	FILE=`echo $1 | sed 's/\//_/g'`
-	touch $FILE
-
-	if git diff --shortstat $1 | grep changed 2>&1  > /dev/null; then
-		printf "\x1b[31mWARNING: file modified\x1b[0m\n"
-	else
-		printf "\n"
-	fi
-
-
-	printf "checking $1\t"
-	echo $FILE
+	echo "Checking $1"
+	TMP=`echo $1 | sed 's/\//_/g'`
+	GITFILE="file_"$TMP
+	sha512sum $1 > $DATABASE/$GITFILE
 }
 
-test_file README.md
-test_file hardening
-test_file database/hardening
+test_directory () {
+	echo "Checking $1"
+	TMP=`echo $1 | sed 's/\//_/g'`
+	GITFILE="directory_"$TMP
+	cat $1/*> $OUTPUT/$GITFILE
+	sha512sum  $OUTPUT/$GITFILE > $DATABASE/$GITFILE
+}
+#*/
 
-exit
+test_permissions () {
+	echo "Checking permissions $1"
+	TMP=`echo $1 | sed 's/\//_/g'`
+	GITFILE="permissions_"$TMP
+	ls -d -l $1 |  awk '{print $1 $9}' > $OUTPUT/$GITFILE
+	sha512sum $OUTPUT/$GITFILE > $DATABASE/$GITFILE
+}
 
+test_rkhunter () {
+	echo "Checking rkhunter, please wait..."
+	GITFILE="rootkit_rkhunter"
+	sudo rkhunter --check --cronjob --disable ipc_shared_mem --report-warnings-only > $OUTPUT/$GITFILE
+	sha512sum $OUTPUT/$GITFILE > $DATABASE/$GITFILE
+}
 
+test_chkrootkit () {
+	echo "Checking chkrootkit, please wait..."
+	GITFILE="rootkit_chkrootkit"
 
+	sudo chkrootkit | grep -v '!' > $OUTPUT/$GITFILE
+	sha512sum $OUTPUT/$GITFILE > $DATABASE/$GITFILE
+}
 
+test_pkgmgr_integrity () {
+	echo "Verifying files (md5sum) installed by dpkg. This will take a few minutes..."
+	GITFILE="pkgmgr_file-integrity"
+	sudo dpkg --verify | tee $OUTPUT/$GITFILE
+	sha512sum $OUTPUT/$GITFILE > $DATABASE/$GITFILE
+}
 
+test_pkgmgr_update ()  {
+	echo "System update"
+	sudo apt-get update
+	sudo apt-get autoremove
+	sudo apt-get -y upgrade
+}
 
-#print_colorized running apt-update
-#sudo apt-get update
-#sudo apt-get autoremove
-#sudo apt-get -y upgrade
-#print_colorized checking dpkg
-#sudo dpkg --verify | tee database/dpkg--verify
+# create database and output directories
+mkdir -p $DATABASE
+mkdir -p $OUTPUT
 
-print_colorized checking .bashrc
-sha512sum ~/.bashrc > database/dot-bashrc
-cp ~/.bashrc > snapshot/.
+test_pkgmgr_update
+test_pkgmgr_integrity
 
-print_colorized checking dot-config-autostart
-cat ~/.config/autostart/* | sha512sum > database/dot-config-autostart
-tar -cJvf snapshot/dot-config-autostart.tar.xz ~/.config/autostart
+# local file tests
+test_file ~/.bashrc
+test_directory ~/.config/autostart
 
-print_colorized checking resolv.conf
-sha512sum /etc/resolv.conf > database/etc-resolv-conf
-cp /etc/resolv.conf > snapshot/.
+# system file tests
+test_file /etc/rc.local
+test_file /etc/fstab
+test_file /etc/resolv.conf
+test_permissions /etc/ssh/sshd_config
+test_permissions /etc/cron.d
+test_permissions /etc/cron.daily
+test_permissions /etc/cron.hourly
+test_permissions /etc/cron.monthly
+test_permissions /etc/crontab
+test_permissions /etc/cron.weekly
 
-print_colorized checking rc.local
-sha512sum /etc/rc.local > database/etc-rc-local
-cp /etc/rc.local > snapshot/.
+# rootkit tests
+test_rkhunter
+test_chkrootkit
 
-print_colorized checking fstab
-sha512sum /etc/fstab > database/etc-fstab
-cp /etc/fstab > snapshot/.
-
-print_colorized checking system-files-permissions
-ls -d -l /etc/cron* /etc/ssh/sshd_config |  awk '{print $1 $9}'' | sha512sum > database/sysfile-permissions
-ls -d -l /etc/cron* /etc/ssh/sshd_config |  awk '{print $1 $9}'' > snapshot/sysfile-permissions
-
-print_colorized checking dns
-/usr/bin/nslookup one.one.one.one | head -n 2 | sha512sum > database/dns
-/usr/bin/nslookup one.one.one.one | head -n 2 > snapshot/dns
-
-print_colorized checking nmap-portscan
-/usr/bin/nmap 127.0.0.1 | grep -v "Starting" | grep -v "latency" | grep -v "scanned" > database/nmap-portscan
-
-print_colorized checking loaded-kernel-modules
-lsmod | cut -f1 -d' ' | sort -u > database/loaded-kernel-modules
-
-print_colorized checking rkhunter
-sudo rkhunter --check --cronjob --disable ipc_shared_mem --report-warnings-only > database/rkhunter
-
-print_colorized checking chkrootkit
-sudo chkrootkit | grep -v '!' > database/chkrootkit
-
-print_colorized all done!
+# closing down
+echo "all done!"
 echo
-print_colorized git status
+echo
 git status
+
+
+# lsmod | cut -f1 -d' ' | sort -u > database/loaded-kernel-modules
